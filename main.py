3ecbec443a855244c9f5aaf9b18dae72bad8f706
postema <pj.postema@gmail.com>
2023-03-18 14:18:11 +0100

    add Title, description, image and language

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -17,7 +18,24 @@ with col2:
     st.info(content)
 
 content2 = """
-Hieronder vind u een aatal apps die ik geschreven heb in Python. 
-U mag natuur ook contact met mij opnemen.
+Hieronder vind u een aantal apps die ik geschreven heb in Python. 
+U mag natuurlijk ook contact met mij opnemen.
 """
 st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"{row['language']} : [Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"{row['language']} : [Source Code]({row['url']})")
